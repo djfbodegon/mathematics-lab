d34680335c9c0ed8ae80a5f397c5a9a72f541bdd
djfbodegon <djfbodegon@gmail.com>
2025-03-18 11:48:20 +0000

    Add 13.rs

---
+++ src/13.rs
@@ -0,0 +1,11 @@
+use std::cmp::Ordering;
+
+fn compare_floats(a: f64, b: f64) -> Ordering {
+    if a < b {
+        Ordering::Less
+    } else if a > b {
+        Ordering::Greater
+    } else {
+        Ordering::Equal
+    }
+}
