0a4b01bb18aa483f4367908c0b3c04b3261c6def
djfbodegon <djfbodegon@gmail.com>
2025-03-31 16:19:33 +0000

    Add 21.rs

---
+++ src/21.rs
@@ -0,0 +1,10 @@
+// This is a simple example of a function that adds two numbers.
+fn main() {
+    let num1 = 4;
+    let num2 = 5;
+
+    // Add the two numbers and store the result in 'result'.
+    let result = num1 + num2;
+
+    println!("The sum of {} and {} is {}", num1, num2, result);
+}
