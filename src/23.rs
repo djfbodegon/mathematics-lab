abfddaef9bbfa5f02a2cad95fde6297bb0ad570d
djfbodegon <djfbodegon@gmail.com>
2025-04-02 00:38:50 +0000

    Add 23.rs

---
+++ src/23.rs
@@ -0,0 +1,16 @@
+fn main() {
+    // Example Rust code to demonstrate basic arithmetic operations
+
+    let num1: i32 = 5;
+    let num2: i32 = 3;
+
+    let sum: i32 = num1 + num2;
+    let product: i32 = num1 * num2;
+    let quotient: i32 = num1 / num2;
+    let remainder: i32 = num1 % num2;
+
+    println!("The sum of {} and {} is {}", num1, num2, sum);
+    println!("The product of {} and {} is {}", num1, num2, product);
+    println!("The quotient of {} and {} is {}", num1, num2, quotient);
+    println!("The remainder of {} divided by {} is {}", num1, num2, remainder);
+}
