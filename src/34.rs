1f6e57465fe99e96f695ff3f4e95ea2b3500346a
djfbodegon <djfbodegon@gmail.com>
2025-04-25 17:49:28 +0000

    Add 34.rs

---
+++ src/34.rs
@@ -0,0 +1,10 @@
+use std::f64;
+
+fn main() {
+    let x = 1.0;
+    let y = 2.0;
+    let z = 3.0;
+
+    println!("The sum of {} and {} is {}", x, y, x + y);
+    println!("The product of {} and {} is {}", x, y, x * y);
+}
