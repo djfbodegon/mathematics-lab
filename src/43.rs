72d0376664ffabb088cd3481a834db8b21098504
djfbodegon <djfbodegon@gmail.com>
2025-05-25 19:57:31 +0000

    Add 43.rs

---
+++ src/43.rs
@@ -0,0 +1,6 @@
+fn main() {
+    // Example of using vectors to store integers
+    let numbers: Vec<i32> = vec![1, 2, 3, 4, 5];
+
+    println!("The vector contains the following elements: {:?}", numbers);
+}
